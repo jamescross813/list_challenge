b6d50754e3ee5d34a8de5854787d05dbed2afe18
jamescross813 <crossjames813@gmail.com>
2023-01-04 15:11:19 -0600

    inc simpler function

--- every_three_numbers.py
+++ every_three_numbers.py
@@ -11,3 +11,5 @@ def every_three_nums(start):
 #Uncomment the line below when your function is done
 print(every_three_nums(91))
 
+# def every_three_nums(start):
+#   return list(range(start, 101, 3))
