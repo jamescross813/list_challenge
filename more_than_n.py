c8614cdf9c6811b70aa0a0a7b9f678072ce5782e
jamescross813 <crossjames813@gmail.com>
2023-01-03 19:33:55 -0600

    basic for loop

--- more_than_n.py
+++ more_than_n.py
@@ -1,5 +1,11 @@
 #Write your function here
+def more_than_n(lst, item, n):
+    lengthlst= len(lst)-1
+    x=0
+    for x in lst:
 
+        if x==len
+            break
 
 #Uncomment the line below when your function is done
 #print(more_than_n([2, 4, 6, 2, 3, 2, 1, 2], 2, 3))
