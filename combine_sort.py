e11ef936f2fd5bd64b73541a5107f4d6f8b53426
jamescross813 <crossjames813@gmail.com>
2023-01-03 19:45:18 -0600

    lsts added and sorted()

--- combine_sort.py
+++ combine_sort.py
@@ -1,6 +1,9 @@
 #Write your function here
 def combine_sort(lst1, lst2):
-
+    newlst = lst1 + lst2
+    return sorted(newlst)
 
 #Uncomment the line below when your function is done
-#print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
+print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
+
+# combine_sort([4, 10, 2, 5], [-10, 2, 5, 10])
