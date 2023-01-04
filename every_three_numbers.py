29621e1f77b5d6d21f5583d76846359ed3c52775
jamescross813 <crossjames813@gmail.com>
2023-01-04 15:07:06 -0600

    wip set up basic control flow and while loop

--- every_three_numbers.py
+++ every_three_numbers.py
@@ -1,6 +1,10 @@
 #Write your function here
 def ever_three_numbers(start):
-    
+    if start== 100:
+        return []
+    i = start
+    while i < 100:
+        i += 3
 
 #Uncomment the line below when your function is done
 # print(every_three_nums(91))
