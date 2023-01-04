70cded9e7d5577cddc63ba86d505d30707ef15b6
jamescross813 <crossjames813@gmail.com>
2023-01-03 18:32:02 -0600

    control flow to compare list lengths:

--- larger_list.py
+++ larger_list.py
@@ -1,6 +1,13 @@
 #Write your function here
 def larger_list(lst1, lst2):
-
+    lst1_length=len(lst1)
+    lst2_length=len(lst2)
+    if(lst1_length>lst2_length):
+        return lst1[-1]
+    elif(lst2_length>lst1_length):
+        return lst2[-1]
+    else:
+        return lst1[-1]
 
 #Uncomment the line below when your function is done
-#print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
+print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
