71a2ec66ab9da877c82969bf6587de6d919b6703
jamescross813 <crossjames813@gmail.com>
2023-01-03 18:25:59 -0600

    comment out more complicated approach

--- append_sum.py
+++ append_sum.py
@@ -1,14 +1,14 @@
-#Write your function here
-def append_sum(lst):
-    length = len(lst)
-    sum = 0
-    x=0
-    for x in lst:
-        sum+=lst[x]
-        x+=1
-        if x == length:
-            break
-    lst.append(sum) 
-    return lst
-#Uncomment the line below when your function is done
-print(append_sum([1, 1, 2]))
+# #Write your function here
+# def append_sum(lst):
+#     length = len(lst)
+#     sum = 0
+#     x=0
+#     for x in lst:
+#         sum+=lst[x]
+#         x+=1
+#         if x == length:
+#             break
+#     lst.append(sum) 
+#     return lst
+# #Uncomment the line below when your function is done
+# print(append_sum([1, 1, 2]))
