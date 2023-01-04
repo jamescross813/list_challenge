8050ce29afb85eb97a41d42a2ca6590fbf185e82
jamescross813 <crossjames813@gmail.com>
2023-01-04 15:11:01 -0600

    working function

--- every_three_numbers.py
+++ every_three_numbers.py
@@ -1,12 +1,13 @@
 #Write your function here
-def ever_three_numbers(start):
+def every_three_nums(start):
     numlst =[]
-    if start== 100:
+    if start > 100:
         return numlst
     i = start
-    while i < 100:
+    while i <= 100:
         numlst.append(i)
         i += 3
     return numlst
 #Uncomment the line below when your function is done
-# print(every_three_nums(91))
+print(every_three_nums(91))
+
