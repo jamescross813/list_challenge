f23f3b0d01b059598f98727e412eaf16134a0341
jamescross813 <crossjames813@gmail.com>
2023-01-03 18:24:51 -0600

    find sum using for loop

--- append_sum.py
+++ append_sum.py
@@ -1,6 +1,13 @@
 #Write your function here
-def appends_sum(lst):
+def append_sum(lst):
+    length = len(lst)
+    sum = 0
+    x=0
+    for x in lst:
+        sum+=lst[x]
+        x+=1
+        if x == length:
+            break
     
-
 #Uncomment the line below when your function is done
-#print(append_sum([1, 1, 2]))
+print(append_sum([1, 1, 2]))
