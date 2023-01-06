382074d2c7e8533db3f3996ad5b739a7318a26cf
jamescross813 <crossjames813@gmail.com>
2023-01-05 20:28:19 -0600

    done

--- middle_num.py
+++ middle_num.py
@@ -1,5 +1,12 @@
 #Write your function here
-
+def middle_element(lst):
+    if len(lst)%2==0:
+        mid = int(len(lst)/2-1)
+        sum = lst[mid] + lst[mid+1]
+        return sum/2
+    else:
+        mid = int(len(lst)/2)
+        return lst[mid]
 
 #Uncomment the line below when your function is done
-#print(middle_element([5, 2, -10, -4, 4, 5]))
+print(middle_element([5, 2, -10, -4, 4, 5]))
