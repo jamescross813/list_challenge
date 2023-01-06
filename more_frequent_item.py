c02927a14930e8f882fef88a23be685314cf2533
jamescross813 <crossjames813@gmail.com>
2023-01-05 19:55:44 -0600

    func w/ args

--- more_frequent_item.py
+++ more_frequent_item.py
@@ -1,4 +1,5 @@
 #Write your function here
-
+def more_frequent_item(lst, item1, item2):
+    
 #Uncomment the line below when your function is done
 #print(more_frequent_item([2, 3, 3, 2, 3, 2, 3, 2, 3], 2, 3))
