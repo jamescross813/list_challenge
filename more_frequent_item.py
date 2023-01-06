e7e1855083e19579a64e95195e6c79a542750495
jamescross813 <crossjames813@gmail.com>
2023-01-05 20:03:57 -0600

    tidy

--- more_frequent_item.py
+++ more_frequent_item.py
@@ -13,11 +13,12 @@ def more_frequent_item(lst, item1, item2):
         return item1
     else:
         return item2
-        
+
 #Uncomment the line below when your function is done
-print(more_frequent_item([2, 3, 3, 2, 3, 2, 3, 2, 3], 2, 3))def more_frequent_item(lst, item1, item2):
-#   def more_frequent_item(lst, item1, item2):
+print(more_frequent_item([2, 3, 3, 2, 3, 2, 3, 2, 3], 2, 3))
+# def more_frequent_item(lst, item1, item2):
 #     if lst.count(item1) >= lst.count(item2):
 #         return item1
 #     else:
 #         return item2
+        
