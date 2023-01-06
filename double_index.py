5ca6c9df3d36815dd88f8744db7530594a463225
jamescross813 <crossjames813@gmail.com>
2023-01-05 20:11:23 -0600

    control flow

--- double_index.py
+++ double_index.py
@@ -1,5 +1,8 @@
 #Write your function here 
 def double_index(lst, index):
-
+    if(index>= len(lst)):
+        return lst
+    else:
+        
 #Uncomment the line below when your function is done
 #print(double_index([3, 8, -10, 12], 2))
