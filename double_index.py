ff989d48661365b64efa29e5e33c09b93f293c40
jamescross813 <crossjames813@gmail.com>
2023-01-05 20:14:58 -0600

    woking function attempt

--- double_index.py
+++ double_index.py
@@ -3,6 +3,9 @@ def double_index(lst, index):
     if(index>= len(lst)):
         return lst
     else:
-        
+        newlst = lst[0:index]
+        newlst.append(lst[index]*2)
+        newlst = newlst + lst[index+1:]
+    return newlst
 #Uncomment the line below when your function is done
-#print(double_index([3, 8, -10, 12], 2))
+print(double_index([3, 8, -10, 12], 2))
